709ae9f6b69b17f9bdbb4031d41e91a3aee8d464
heba12377 <hebaelsaidali@gmail.com>
2023-03-11 19:06:51 +0200

    Add project files.

---
+++ Day1/Controllers/CarController1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Day1.Models;
+namespace Day1.Controllers
+{
+    public class CarController1 : Controller
+    {
+        public IActionResult getAll()
+        {
+            var cars = Car.GetCars();
+            return View(cars);
+        }
+
+        public IActionResult getDetails(string man, string from)
+        {
+            Data data= new Data();
+            var car = Car.GetCars().FirstOrDefault(c=>c.Manufacturer ==man);
+
+            data.car = car;
+            data.Status = from;
+
+            return View(data);
+        }
+    }
+}
---
+++ Day1/Models/Data.cs
@@ -0,0 +1,9 @@
+﻿namespace Day1.Models
+{
+   
+    public class Data
+    {
+        public Car? car { get; set; }
+        public string? Status { get; set; }
+    }
+}
